cad888c390ac78b6446fb14946ab698eae514874
Dream <138191954+rajiv2004-cloud@users.noreply.github.com>
2024-01-31 20:46:04 +0530

    added thread demo 1

---
+++ proj-1/src/MultiThreading.java
@@ -0,0 +1,21 @@
+class MultiThreading extends Thread{
+    public void run(){
+        try {
+            System.out.println("Thread "+ Thread.currentThread().getId() + "is running");
+        }
+        catch (Exception e){
+            System.out.println("Exception caught");
+        }
+    }
+}
+
+//main class
+class Multithread{
+    public static void main(String[] args){
+        int n= 8;
+        for (int i = 0; i < n; i++){
+            MultiThreading obj = new MultiThreading();
+            obj.start();
+        }
+    }
+}
