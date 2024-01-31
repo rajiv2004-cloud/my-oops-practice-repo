65aacfa5bcbe7a7467a5de1298ee8a709e9d8d05
Dream <138191954+rajiv2004-cloud@users.noreply.github.com>
2024-01-31 11:48:31 +0530

    Added inheritance

---
+++ proj-1/src/inheritanceprint.java
@@ -0,0 +1,22 @@
+interface inheritanceprint {
+    void print();
+}
+interface show {
+    void show1();
+}
+class A implements inheritanceprint, show{
+    @Override
+    public void print() {
+        System.out.println("Hii");
+    }
+
+    @Override
+    public void show1() {
+        System.out.println("Guys");
+    }
+    public static void main(String args[]){
+        A obj = new A();
+        obj.print();
+        obj.show1();
+    }
+}
